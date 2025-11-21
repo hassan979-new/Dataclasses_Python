84c4e1d6a56c150527bb51bce70c7ccd6fbd3bfb
hassan979-new <hagouram979@gmai.com>
2025-11-21 14:00:02 +0100

    changer le code dans main.py pour meilleur affichage

--- Exercice1/main.py
+++ Exercice1/main.py
@@ -4,7 +4,9 @@ livre1 = Livre("1984", "George Orwell", 1949, 9.90)
 livre_json = livre1.to_json()
 livre2 = charger_json(livre_json)
 livre3 = livre1.promo(7.50)
-print(livre1)
-print(livre2)
-print(livre3)
+
 print(livre3<livre2)
+
+print(livre1.to_json())
+print(livre2.to_json())
+print(livre3.to_json())
